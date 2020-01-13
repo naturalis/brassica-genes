b0d6e1b44759dc13124feb0711b54386974e6503
hfrijmann <55240504+hfrijmann@users.noreply.github.com>
2020-01-13 13:04:20 +0100

    Update blast_selector.py
    Added a selection method to the programm, it should now be fully functional. To be tested by Nino this afternoon, new upload should follow before the end of tuesday (14-1-2020) morning.

--- script/blast_selector.py
+++ script/blast_selector.py
@@ -3,6 +3,7 @@
 
 from os import listdir
 from os.path import isfile
+from os import remove
 
 
 def list_blast_files(dir):
@@ -23,30 +24,67 @@ def list_blast_files(dir):
 
 
 def read_blast_file(location):
+    """
+
+    :param location: String containing the exact relative filepath to
+        the BLAST file to be read.
+    :return blast_result: A 2D-list; contains lists with a variety of
+        Int, float and string elements detailing all results of a
+        single BLAST result record.
+    """
     with open(location, 'r') as infile:
         content = infile.readlines()
     blast_result = []
     for line in content:
         if line[0] != "#":
             line = line.rstrip("\n").split("\t")
-            line[2] = float(line[2])
-            for pos in range(3, 10):
-                line[pos] = int(line[pos])
-            line[11] = int(line[11])
+            # temporarily commented out for testing!
+            # line[2] = float(line[2])
+            # for pos in range(3, 10):
+            #    line[pos] = int(line[pos])
+            # line[11] = int(line[11])
         blast_result.append(line)
     return blast_result
+
+
+def select_chrom(results):
+    query = results[0]
+    blast_results = []
+    for record in results:
+        if query[2] == record[2]:
+            blast_results.append(record)
+    return results
 
 
 if __name__ == "__main__":
     # Make it easier for the argument parsing
     blast_dr = "blast_resultaten/"
 
+    # Prepare output file. Clear if exists; add header
+    if isfile(dir+"target_gene_locations.txt"):
+        remove(dir+"target_gene_locations.txt")
+    with open(dir+"target_gene_locations.txt", 'a') as outfile:
+        outfile.write("filename\tcontig\tstart\tend\n")
+
     # List all subject BLAST files
     blast_files = list_blast_files(blast_dr)
 
     # Loop over BLAST files
     for file in blast_files:
-        # Open the current BLAST file:
-        result = read_blast_file(dir+file)
-
-
+        # Open the current BLAST file
+        blast_results = read_blast_file(dir+file)
+        # If there is a BLAST result, start filtering
+        if blast_results != []:
+            blast_results = select_chrom(blast_results)
+        if blast_results != []:
+            # BLAST sorting guarantees highest bit-score comes first
+            final_result = blast_results[0]
+            with open(dir+"target_gene_locations.txt" 'a') as outfile:
+                outfile.write(
+                    "%s\t%s\t%s\t%s\n"%(file,
+                                        blast_results[1],
+                                        blast_results[8],
+                                        blast_results[9],)
+                )
+        if blast_results == []:
+            print("No BLAST results found for file: %s"%(file))
