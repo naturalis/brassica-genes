4d7e51c0f35fc9cc79b06dc97f025c5c2a8b3ee0
hfrijmann <s1101245@student.hsleiden.nl>
2020-01-14 08:55:31 +0000

    Added pipeline.sh. This shell script calls all project relevant scripts in the proper order, allowing for consistent and time-efficient reruns and testing of the project's scripts.

---
+++ script/pipeline.sh
@@ -0,0 +1,16 @@
+bash script_shortlist.sh
+python3 slice_script.py
+bash blast.sh
+python3 blast_selector.py
+python3 slice_script.py
+
+
+
+
+#Blasten met de geslicte sequenties
+#Blast resultaat filteren op hits die dicht bij de sequenties die eerder zijn gesliced liggen
+	#De locaties gebruiken om het slice script weer te aan te roepen
+	#De nieuw geslicte sequenties onder de eerdere zetten
+	#Alignen
+	#Tellen van de SNPS
+
