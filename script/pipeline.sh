8145ea3842b418464d18f34639f463ff0bd8c863
hfrijmann <s1101245@student.hsleiden.nl>
2020-01-25 14:01:12 +0000

    updated scripts; they now have infile documentation.

--- script/pipeline.sh
+++ script/pipeline.sh
@@ -3,14 +3,9 @@ python3 slice_script.py
 bash blast.sh
 python3 blast_selector.py
 python3 slice_script.py
-
-
-
-
-#Blasten met de geslicte sequenties
-#Blast resultaat filteren op hits die dicht bij de sequenties die eerder zijn gesliced liggen
-	#De locaties gebruiken om het slice script weer te aan te roepen
-	#De nieuw geslicte sequenties onder de eerdere zetten
-	#Alignen
-	#Tellen van de SNPS
+python3 translator.py
+bash alignments.sh
+python3 variant_caller.py
+bash total_variance.sh
+python3 tsv_creator.py
 
