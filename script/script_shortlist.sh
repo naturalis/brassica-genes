24daa840f2fdb51e8cceb9bdf91ae950708afb97
hfrijmann <s1101245@student.hsleiden.nl>
2020-01-25 14:02:33 +0000

    updated scripts; they now have infile documentation.

--- script/script_shortlist.sh
+++ script/script_shortlist.sh
@@ -1,15 +1,16 @@
-#Het maken van de shortlist.
-gunzip -c /home/bpexa/data/Brassica_oleracea.v2.1.39.chr.gff3.gz | more | awk '{print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7"\t"$8"\t"$9}' > vergelijk_lijst.txt
-cat BPEXA_genes.tsv | awk {'print $2'} | sed '$d' > genID.txt
-egrep -f vergelijk_lijst.txt > gff3_shortlist.txt
+### Documentation
+#This program filters the gff3 of the whole genome into a smaller list. The shortlist contains the gene of interest.
+gunzip -c /home/bpexa/data/Brassica_oleracea.v2.1.39.chr.gff3.gz | more | awk '{print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7"\t"$8"\t"$9}' > comparison.txt #Saves necessary columns in vergelijk_lijst.txt
+cat BPEXA_genes.tsv | awk {'print $2'} | sed '$d' > genID.txt #Saves only the gene names into genID.txt
+egrep -f genID.txt comparison.txt > gff3_shortlist.txt #Genenames are used as a search query in comparison.txt and saves the filtered shortlist in gff3_shortlist.txt
 
-##Validatie stappen
-#1. Invertarisatie van de shortlist
+
+##Additional validation steps
+#1. Invertarisation of the shortlist
 #cat ~/script/github/resultaten_project/shortlist/gff3_shortlist.txt | awk '{print $2}' | sort | uniq -c
 
-#2. Duplicaten check
+#2. Duplicate check
 #cat ~/script/github/resultaten_project/shortlist/gff3_shortlist.txt | sort | uniq -c
 
-#3. Hoeveel unieke genen zitten er in de aangeleverde genen lijst. En komt dit aantal overeen met wat in stap 1 is geconstateerd?
+#3. Amount of unique genes present in the shortlist. Does this corresponds with step 1?
 #cat ~/script/github/resultaten_project/shortlist/genID.txt | sort | uniq | wc -l
-
