20960a9353ff203ab6f0e7410b15e2c92b5cc8d8
hfrijmann <s1101245@student.hsleiden.nl>
2020-01-25 14:00:49 +0000

    updated scripts; they now have infile documentation.

--- script/alignments.sh
+++ script/alignments.sh
@@ -1,26 +1,31 @@
+##Documentation
+#This program automatically performs all alignments. Nucleotide and Amino acids alingments need to be performed.
+
 rm -f alignments/*
+rm -f alignments_translated/*
 mkdir alignments
+mkdir alignments_translated
+
 cd fasta_resultaten
-fa=$( ls | wc -l )
-for ((i=1;i<=${fa};i=i+1))
+fa=$( ls | wc -l ) #Determine how many files are present in the file.
+for ((i=1;i<=${fa};i=i+1)) #Loop over all files.
 do
 {
-	file=$(ls | head -n ${i} | tail -n 1)
-	file2=${file%.*}
-        muscle -in ${file} -out ../alignments/$file2.al
+	file=$(ls | head -n ${i} | tail -n 1) #Fetching filename
+	file2=${file%.*} #Remove the original extension. 
+        muscle -in ${file} -clwout ../alignments/$file2.aln #Performing the actual alignment. Creating .aln files.
 }
 done
 
-cd ../translated_fasta
-
+cd ../translated_fasta #Switch to correct directory
 fa=$( ls | wc -l )
 for ((i=1;i<=${fa};i=i+1))
 do
 {
         file=$(ls | head -n ${i} | tail -n 1)
         file2=${file%.*}
-        muscle -in ${file} -out ../alignments_translated/$file2.al
+        muscle -in ${file} -clwout ../alignments_translated/$file2.aln
 }
 done
 
-
+cd ..
