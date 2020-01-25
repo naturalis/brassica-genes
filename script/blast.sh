ef861809f26bf03780bc9caf26b287d8253a9b3a
hfrijmann <s1101245@student.hsleiden.nl>
2020-01-25 14:00:58 +0000

    updated scripts; they now have infile documentation.

--- script/blast.sh
+++ script/blast.sh
@@ -1,23 +1,24 @@
-#Documentation
+###Documentation
 #This program uses FASTA formatted sequences originated from the slice script earlier in the pipeline.
-#First of all a nucleotide database is created of the target genome.
-#A loop is created over the directory that contains these FASTA files.
 #Each individual FASTA file is used by the BLAST algorithm to detect the most similar sequences.
 
+#A nucleotide database for the target genome is created.
 makeblastdb -in jerseykale_consensus.fa -dbtype nucl
 
+#If the directory already exists, it gets removed. 
 rm -f blast_resultaten/*
 mkdir -p  blast_resultaten
 
-fa=$( ls fasta_resultaten | wc -l )
-for ((i=1;i<=${fa};i=i+1))
+
+fa=$( ls fasta_resultaten | wc -l ) #Determine the amount of files present in the directory
+for ((i=1;i<=${fa};i=i+1))          #A loop is created in the directory that contains these FASTA files.
 do
 {
-        file=$(ls fasta_resultaten | head -n ${i} | tail -n 1)
+        file=$(ls fasta_resultaten | head -n ${i} | tail -n 1) #Method to select 1 file at the time
         file2=${file%.*}
-        blastn -query fasta_resultaten/${file} -db jerseykale_consensus.fa -out blast_resultaten/${file2}.blast -outfmt 7
+        blastn -query fasta_resultaten/${file} -db jerseykale_consensus.fa -out blast_resultaten/${file2}.blast -outfmt 7  #Each individual FASTA file is used by
+                                                                                                                           #the BLAST algorithm to detect the most similar sequences.
 }
 done
 
-#Het verwerken van een output file. We hebben nodig: GenID, op welk chromosoom ligt het, en waar op dat chromosoom.
-# cat output.txt | egrep -v [#] | awk {'print $1"\t"$2"\t"$9"\t"$10'} > output_gefilterd.txt #Alle bruikbare informatie is nu verzameld vanuit de output file
+
